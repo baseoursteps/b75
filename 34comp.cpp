df3dca06ff12370bff6a0bfcaaeac18858013f56
​ <>
2021-12-31 16:15:14 +0200

    graph components

---
+++ 34comp.cpp
@@ -0,0 +1,68 @@
+#include <array>
+#include <cstddef>
+#include <iostream>
+#include <memory>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#include "graph.h"
+
+using namespace std;
+
+// given a list of edges in an undirected graph count the connected components
+
+using edge = array<size_t, 2>;
+
+// we need a sort of dfs where we mark elements as we visit them
+// after we visit them and we still have unvisited it means that we start a new
+// component
+
+template<typename T>
+void
+dfs(T elem, unordered_map<T, typename Graph<T>::Owned> &vals)
+{
+    if (!vals.count(elem))
+        return;
+
+    auto neigh = std::move(vals.at(elem)->neigh);
+    vals.erase(elem);
+
+    for (auto &&n : neigh)
+        if (auto l = n.lock())
+            dfs(l->val, vals);
+}
+
+template<typename T>
+size_t
+count_comp(typename Graph<T>::Edges &edges)
+{
+    T count {};
+
+    auto vals = Graph<T>::from_edges(edges);
+
+    while (!vals.empty()) {
+        auto elem = vals.begin();
+        count++;
+        dfs(elem->second->val, vals);
+    }
+
+    return count;
+}
+
+int
+main()
+{
+    // 2 components
+    Graph<size_t>::Edges edges0 { { 0, 1 }, { 1, 2 }, { 3, 4 } };
+
+    // 1
+    Graph<size_t>::Edges edges1 { { 0, 1 }, { 1, 2 }, { 3, 2 } };
+
+    // 3
+    Graph<size_t>::Edges edges2 { { 0, 1 }, { 1, 0 }, { 2, 1 },
+                                  { 0, 2 }, { 3, 4 }, { 5, 6 } };
+
+    cout << count_comp<size_t>(edges0) << count_comp<size_t>(edges1)
+         << count_comp<size_t>(edges2) << "\n";
+}
--- graph.h
+++ graph.h
@@ -1,18 +1,21 @@
 #pragma once
 
+#include <array>
+#include <cstddef>
 #include <deque>
 #include <iostream>
 #include <memory>
 #include <ostream>
+#include <unordered_map>
 #include <unordered_set>
 #include <vector>
 
-// TODO -- break cycle in order to permit graph to deallocate
 template<typename T>
 struct Graph
 {
     using Neigh = std::vector<std::weak_ptr<Graph<T>>>;
     using Owned = std::shared_ptr<Graph<T>>;
+    using Edges = std::vector<std::array<T, 2>>;
 
     T     val;
     Neigh neigh;
@@ -20,6 +23,25 @@ struct Graph
     Graph<T>() : val { {} } {}
     Graph<T>(const T &val) : val(val) {}
     Graph<T>(const T &val, const Neigh &neigh) : val(val), neigh(neigh) {}
+
+    static std::unordered_map<T, typename Graph<T>::Owned>
+    from_edges(const Graph<T>::Edges &edges, bool directed = false)
+    {
+        std::unordered_map<T, Graph<T>::Owned> elems;
+
+        for (auto &&e : edges) {
+            elems.insert({ e.at(0), std::make_shared<Graph<T>>(e.at(0)) });
+            elems.insert({ e.at(1), std::make_shared<Graph<T>>(e.at(1)) });
+        }
+
+        for (auto &&e : edges) {
+            elems.at(e.at(0))->neigh.push_back(elems.at(e.at(1)));
+            if (!directed)
+                elems.at(e.at(1))->neigh.push_back(elems.at(e.at(0)));
+        }
+
+        return elems;
+    }
 
     friend std::ostream &
     operator<<(std::ostream &out, Owned &g)
@@ -36,7 +58,7 @@ struct Graph
             auto c = all.front();
             all.pop_front();
 
-            if (visited.find(c) != visited.end())
+            if (!visited.count(c))
                 continue;
 
             visited.insert(c);
@@ -47,7 +69,7 @@ struct Graph
                 if (auto nl = n.lock()) {
                     out << nl->val << " ";
 
-                    if (visited.find(nl) == visited.end())
+                    if (!visited.count(nl))
                         all.push_back(nl);
                 }
             }
